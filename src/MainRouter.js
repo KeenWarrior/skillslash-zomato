cf603755fdd4036cd6419ecfb520a72092a55677
Anuj Garg <anuj@Anujs-MacBook-Pro.local>
2023-05-06 11:06:38 +0530

    Initial code

---
+++ src/MainRouter.js
@@ -0,0 +1,68 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainBottomNavigation from "./MainBottomNavigation";
+import OrderPage from "./pages/OrderPage";
+import LoginPage from "./pages/LoginPage";
+import UserGaurd from "./gaurds/UserGaurd";
+import AuthGaurd from "./gaurds/AuthGaurd";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <AuthGaurd>
+        <OrderPage />
+      </AuthGaurd>
+    ),
+  },
+  {
+    path: "/goout",
+    element: (
+      <AuthGaurd>
+        <div>
+          Go out
+          <MainBottomNavigation />
+        </div>
+      </AuthGaurd>
+    ),
+  },
+  {
+    path: "/gold",
+    element: (
+      <div>
+        Gold
+        <MainBottomNavigation />
+      </div>
+    ),
+  },
+  {
+    path: "/search",
+    element: (
+      <div>
+        Search
+        <MainBottomNavigation />
+      </div>
+    ),
+  },
+  {
+    path: "/profile",
+    element: (
+      <div>
+        Profile
+        <MainBottomNavigation />
+      </div>
+    ),
+  },
+
+  {
+    path: "/login",
+    element: (
+      <UserGaurd>
+        <LoginPage />
+      </UserGaurd>
+    ),
+  },
+]);
+
+export default function MainRouter() {
+  return <RouterProvider router={router} />;
+}
